3e6e5658eb8c088cfd59bb67d41774f7ad158606
NicolasArcelin <103464938+NicolasArcelin@users.noreply.github.com>
2022-08-29 17:40:01 -0400

    Dynamic Seach Field Created & Optimized

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { Component } from "react";
 
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   constructor() {
+    console.log("constructor");
     super();
     this.state = {
       monsters: [],
+      searchString: "",
     };
   }
 
   componentDidMount() {
+    console.log("componentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
@@ -23,10 +25,32 @@ class App extends Component {
       );
   }
 
+  onSearchChange = (event) => {
+    console.log(event.target.value);
+    const searchString = event.target.value.toLowerCase();
+
+    this.setState(() => {
+      return { searchString };
+    });
+  };
+
   render() {
+    console.log("render");
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchString);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <h1>Title</h1>
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search Monsters"
+          onChange={this.onSearchChange}
+        />
+        {filteredMonsters.map((monster) => {
           return <h1 key={monster.id}>{monster.name}</h1>;
         })}
       </div>
